d4f56749b7a9019c7d2ece2558139f1362f40112
Wolodja Wentland <wentland@cl.uni-heidelberg.de>
2009-12-04 16:44:52 +0100

    Clean mwdb.build_info

--- lib/mwdb/build_info.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-DATA_DIR = '/home/babilen/.virtualenvs/py3/share'
-LIB_DIR = '/home/babilen/.virtualenvs/py3/lib/python3.1/site-packages/'
-SCRIPT_DIR = '/home/babilen/.virtualenvs/py3/bin'
